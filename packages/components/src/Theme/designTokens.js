8bb5d240994897f51bd8bc9db89eb26bfbfd10c3
Kelly Willard <kellyw@kellywiardmbp16.lan>
2020-07-27 12:09:32 -0700

    build design tokens

--- packages/components/src/Theme/designTokens.js
+++ packages/components/src/Theme/designTokens.js
@@ -75,7 +75,7 @@ const defaultDesignTokens = {
     },
     body1: {
       fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
-      fontWeight: 400,
+      fontWeight: 300,
       fontSize: "1rem",
       lineHeight: 1.5,
       letterSpacing: "0.00938em"
